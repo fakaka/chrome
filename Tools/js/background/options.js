43c1ef97c0f8bdbd78e15c66566723a22ba690d6
hairpin <vsmj012@163.com>
2019-04-22 14:29:44 +0800

    删除不必要的文件 修改样式

--- Tools/js/background/options.js
+++ Tools/js/background/options.js
@@ -1,4 +1,4 @@
-var qrcode = new QRCode(document.getElementById('qrcode'), {
+var qrcodeDemo = new QRCode(document.getElementById('qrcode'), {
     text: 'http://jindo.dev.naver.com/collie',
     width: 128,
     height: 128,
